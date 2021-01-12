2c33e5c28aa9d00d332383f9ef33b759adeccf7b
josue-avila <josue.avila@olist.com>
2021-01-12 16:52:22 -0300

    adjusting frontend behavior

--- frontend/product.py
+++ frontend/product.py
@@ -1,6 +1,7 @@
 from flask import request, render_template, Blueprint
 
 from backend.controllers.product import read_product, write_product
+from backend.models.product import Product
 
 product = Blueprint(__name__, 'product')
 
@@ -10,11 +11,12 @@ def register_product():
     product_name = request.args.get('produto')
     description = request.args.get('descricao')
     price = request.args.get('preco')
+    product = Product(product_name, description, price)
 
     if (product_name is None) and (description is None) and (price is None):
         pass
     else:
-        write_product(product_name, description, price)
+        write_product(product)
     return render_template('products.html', name='olist')
 
 
