207b6a4131c6aa53b982ac4971279659dae680d1
Matheus Abreu <matheusjakeabreu@gmail.com>
2021-01-12 17:22:47 -0300

    Resolve conflicts

--- backend/controllers/category.py
+++ backend/controllers/category.py
@@ -7,11 +7,7 @@ from backend.models.category import Category
 def write_category(name: str, description: str) -> None:
     category = Category(name, description)
     write(category)
-<<<<<<< HEAD
-    insert_log("Inserted", f"Category - {name}")
-=======
     create_log("Inserted", f"Category - {name}")
->>>>>>> 74fc270c90dd064fda6d80fb693bcf17680dd386
 
 
 def read_categories() -> list:
