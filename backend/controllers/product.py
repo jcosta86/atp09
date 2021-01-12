a31623841c4f7fae34d755f3fa26c81aa0ad1c96
Matheus Abreu <matheusjakeabreu@gmail.com>
2021-01-12 17:24:21 -0300

    Resolved conflicts

--- backend/controllers/product.py
+++ backend/controllers/product.py
@@ -3,17 +3,10 @@ from backend.dao.product_dao import write, read
 from backend.models.product import Product
 
 
-<<<<<<< HEAD
-def write_product(name: str, description, price: float) -> None:
+def write_product(name: str, description: str, price: float) -> None:
     product = Product(name, description, price)
     write(product)
-    insert_log('Inserted', f'Product - {name}')
-=======
-
-def write_product(product: Product) -> None:
-    write(product)
     create_log('Inserted', f'Product - {name}')
->>>>>>> 74fc270c90dd064fda6d80fb693bcf17680dd386
 
 
 def read_product() -> list:
