d925bdfbc2707710f3f59449b683aaa6b08e4f14
Matheus Abreu <matheus.abreu@olist.com>
2021-01-13 11:13:41 -0300

    Renamed controllers

--- backend/controllers/product_controller.py
+++ backend/controllers/product_controller.py
@@ -1,15 +1,15 @@
-from backend.controllers.log import create_log
+from backend.controllers.log_controller import write_log
 from backend.dao.product_dao import write, read
-from backend.models.product import Product
+from backend.models.product_model import Product
 
 
 def write_product(name: str, description: str, price: float) -> None:
     product = Product(name, description, price)
     write(product)
-    create_log('Inserted', f'Product - {name}')
+    write_log('Inserted', f'Product - {name}')
 
 
 def read_product() -> list:
     products = read()
-    create_log('List', 'Product')
+    write_log('List', 'Product')
     return products
--- backend/controllers/seller_controller.py
+++ backend/controllers/seller_controller.py
@@ -1,15 +1,15 @@
-from backend.controllers.log import create_log
+from backend.controllers.log_controller import write_log
 from backend.dao.seller_dao import write, read
-from backend.models.seller import Seller
+from backend.models.seller_model import Seller
 
 
 def write_seller(fullname: str, email: str, phone: str) -> None:
     seller = Seller(fullname, email, phone)
     write(seller)
-    create_log("Inserted", f"Seller - {seller.fullname}")
+    write_log("Inserted", f"Seller - {seller.fullname}")
 
 
 def read_seller() -> list:
     sellers = read()
-    create_log("List", "Seller")
+    write_log("List", "Seller")
     return sellers
