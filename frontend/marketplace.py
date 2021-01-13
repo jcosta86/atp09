6f358d4131f5eec1768ef35af3622fcb68a6d403
Matheus Abreu <matheus.abreu@olist.com>
2021-01-13 14:45:37 -0300

    Implemented update and delete

--- frontend/marketplace.py
+++ frontend/marketplace.py
@@ -1,6 +1,7 @@
-from flask import request, render_template, Blueprint
+from flask import request, render_template, Blueprint, redirect
 
-from backend.controllers.marketplace_controller import read_marketplace, write_marketplace
+from backend.controllers.marketplace_controller import read_marketplace, write_marketplace, update_marketplace, \
+    delete_marketplace
 
 marketplace = Blueprint(__name__, 'marketplace')
 
@@ -13,6 +14,7 @@ def register_marketplace():
         pass
     else:
         write_marketplace(marketplace_name, description)
+        return redirect('/list_marketplaces')
     return render_template('marketplaces.html', name='olist')
 
 
@@ -20,3 +22,28 @@ def register_marketplace():
 def list_marketplaces():
     marketplaces = read_marketplace()
     return render_template('list_marketplaces.html', marketplaces=marketplaces)
+
+
+@marketplace.route('/marketplaces/update')
+def marketplace_update():
+    id = request.args.get('id')
+    marketplace_name = request.args.get('marketplace')
+    description = request.args.get('descricao')
+    return render_template('marketplaces.html', name='olist', edit=True, id=id, marketplace_name=marketplace_name,
+                           description=description)
+
+
+@marketplace.route('/marketplaces/update', methods=['POST'])
+def save_marketplace_update():
+    id = request.form.get('id')
+    name = request.form.get('marketplace')
+    description = request.form.get('descricao')
+    update_marketplace(id, name, description)
+    return redirect('/list_marketplaces')
+
+
+@marketplace.route('/marketplaces/delete')
+def marketplace_delete():
+    id = request.args.get('id')
+    delete_marketplace(id)
+    return redirect('/list_marketplaces')
--- frontend/seller.py
+++ frontend/seller.py
@@ -1,6 +1,6 @@
-from flask import request, render_template, Blueprint
+from flask import request, render_template, Blueprint, redirect
 
-from backend.controllers.seller_controller import read_seller, write_seller
+from backend.controllers.seller_controller import read_seller, write_seller, update_seller, delete_seller
 
 seller = Blueprint(__name__, 'seller')
 
@@ -14,6 +14,7 @@ def register_seller():
         pass
     else:
         write_seller(full_name, email, phone)
+        return redirect('/list_sellers')
     return render_template('sellers.html', name='olist')
 
 
@@ -21,3 +22,29 @@ def register_seller():
 def list_sellers():
     sellers = read_seller()
     return render_template('list_sellers.html', sellers=sellers)
+
+
+@seller.route('/sellers/update')
+def seller_update():
+    id = request.args.get('id')
+    full_name = request.args.get('name')
+    email = request.args.get('email')
+    phone = request.args.get('phone_number')
+    return render_template('sellers.html', name='olist', edit=True, id=id, fullname=full_name, email=email, phone=phone)
+
+
+@seller.route('/sellers/update', methods=['POST'])
+def save_seller_update():
+    id = request.form.get('id')
+    full_name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone_number')
+    update_seller(id, full_name, email, phone)
+    return redirect('/list_sellers')
+
+
+@seller.route('/sellers/delete')
+def seller_delete():
+    id = request.args.get('id')
+    delete_seller(id)
+    return redirect('/list_sellers')
