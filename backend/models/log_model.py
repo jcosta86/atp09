a6fb3d9e0d837953eff5d2edf64a3f1515ff8431
Matheus Abreu <matheus.abreu@olist.com>
2021-01-14 17:33:10 -0300

    Log model Refactor WIP

--- backend/models/log_model.py
+++ backend/models/log_model.py
@@ -1,4 +1,13 @@
-class Log:
+from backend.models.base_model import BaseModel
+
+
+class Log(BaseModel):
+    __tablename__ = 'logfile'
+    id = None
+    activity = str()
+    date_activity = str()
+    time_activity = str()
+
     def __init__(self, activity: str, domain_activity: str, id: int = None, date_activity: str = None,
                  time_activity: str = None):
         self.id = id
