08d73f382c74045ea0f14a0cbb39468152a0c2ec
Matheus Abreu <matheus.abreu@olist.com>
2021-01-14 11:15:56 -0300

    Refactored database connection to custom class

--- backend/dao/database.py
+++ backend/dao/database.py
@@ -1,24 +1,15 @@
-import os
-
-import psycopg2
-
-HOST = os.getenv('DB_HOST')
-USER = os.getenv('DB_USER')
-PASSWORD = os.getenv('DB_PASSWORD')
-DATABASE = os.getenv('DB_NAME')
-
-CONNECTION_STRING = f"host={HOST} user={USER} dbname={DATABASE} password={PASSWORD}"
+from backend.dao.connection import Connection
 
 
 def execute_query(query: str) -> None:
-    with psycopg2.connect(CONNECTION_STRING) as connection:
+    with Connection() as connection:
         with connection.cursor() as cursor:
             cursor.execute(query)
             connection.commit()
 
 
 def select_query(query: str) -> list:
-    with psycopg2.connect(CONNECTION_STRING) as connection:
+    with Connection() as connection:
         with connection.cursor() as cursor:
             cursor.execute(query)
             result = cursor.fetchall()
