56774a474c25646cad53985193109f0c6ff94ca9
Matheus Abreu <matheus.abreu@olist.com>
2021-01-14 17:32:43 -0300

    Category model Refactor

--- backend/models/category_model.py
+++ backend/models/category_model.py
@@ -1,4 +1,12 @@
-class Category:
+from backend.models.base_model import BaseModel
+
+
+class Category(BaseModel):
+    __tablename__ = 'category'
+    id = None
+    name = str()
+    description = str()
+
     def __init__(self, name: str, description: str, id: int = None) -> None:
         self.id = id
         self.name = name
