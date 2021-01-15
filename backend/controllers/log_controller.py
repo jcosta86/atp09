b0a8c317233a429a1214723be656d87dfbfadb40
josue-avila <josue.avila@olist.com>
2021-01-15 11:51:07 -0300

    implementing base controller

--- backend/controllers/log_controller.py
+++ backend/controllers/log_controller.py
@@ -3,12 +3,7 @@ from backend.models.log_model import Log
 
 DAO = LogDao()
 
-
-def write_log(activity, domain_activity):
-    log = Log(activity, domain_activity)
-    DAO.write(log)
-
-
-def read_log() -> list:
-    log = DAO.read()
-    return log
+class LogController:
+    def read_log(self) -> list:
+        log = DAO.read()
+        return log
--- backend/controllers/marketplace_controller.py
+++ backend/controllers/marketplace_controller.py
@@ -1,28 +1,8 @@
-from backend.controllers.log_controller import write_log
-from backend.dao.marketplace_dao import MarketplaceDao
-from backend.models.marketplace_model import Marketplace
+from backend.dao.marketplace_dao import MarketplaceDao 
+from backend.controllers.base_controller import BaseController
 
 DAO = MarketplaceDao()
 
-
-def write_marketplace(name: str, description: str) -> None:
-    marketplace = Marketplace(name, description)
-    DAO.write(marketplace)
-    write_log("Inserted", f"Marketplace - {name}")
-
-
-def read_marketplace() -> list:
-    marketplaces = DAO.read()
-    write_log('List', 'Marketplace')
-    return marketplaces
-
-
-def update_marketplace(id: int, name: str, description: str) -> None:
-    marketplace = Marketplace(id=id, name=name, description=description)
-    DAO.update(marketplace)
-    write_log("Updated", f"Marketplace - ID: {id}")
-
-
-def delete_marketplace(id: int) -> None:
-    DAO.delete(id)
-    write_log("Deleted", f"Marketplace - ID: {id}")
+class MarketplaceController(BaseController):
+    def __init__(self):
+        super().__init__( DAO, 'Marketplace')
