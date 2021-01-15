71cbd0499ecee4c127ac05e7dd6ddbdb936ae8ce
Matheus Abreu <matheus.abreu@olist.com>
2021-01-14 23:42:13 -0300

    Finished BaseDao implementation

--- backend/dao/base_dao.py
+++ backend/dao/base_dao.py
@@ -1,4 +1,6 @@
-from backend.dao.database import select_query, execute_query
+from psycopg2.extras import DictCursor, DictRow
+
+from backend.dao.connection import Connection
 from backend.models.base_model import BaseModel
 
 
@@ -7,57 +9,85 @@ class BaseDao:
         self.model = model
         self.table_name = self.model.__tablename__
 
-    def _get_db_attributes_names(self) -> str:
+    @staticmethod
+    def execute_query(query: str) -> None:
+        with Connection() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query)
+                connection.commit()
+
+    @staticmethod
+    def select_all_query(query: str) -> list:
+        with Connection() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                connection.commit()
+        return result
+
+    @staticmethod
+    def select_one_query(query: str) -> DictRow:
+        with Connection() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query)
+                result = cursor.fetchone()
+                connection.commit()
+        return result
+
+    def _get_db_columns_names(self) -> str:
         separator = ', '
         model_attributes = vars(self.model)
-        db_attributes = [key for key, value in model_attributes.items() if not key.startswith('__')]
-        return separator.join(db_attributes)
+        columns_names = [key for key, value in model_attributes.items() if not key.startswith('__')]
+        return separator.join(columns_names)
 
-    def _get_db_attributes_names_without_id(self) -> str:
+    def _get_db_columns_names_without_id(self) -> str:
         separator = ', '
         model_attributes = vars(self.model)
-        db_attributes = [key for key, value in model_attributes.items() if not key.startswith('__') and 'id' not in key]
-        return separator.join(db_attributes)
+        columns_names = [key for key, value in model_attributes.items() if not key.startswith('__') and 'id' not in key]
+        return separator.join(columns_names)
 
-    def _generate_write_values_string(self, instance) -> str:
+    def _generate_write_values_string(self, instance: BaseModel) -> str:
         separator = ', '
-        model_attributes = vars(instance)
-        db_attributes = [f"'{value}'" for key, value in model_attributes.items() if
+        instance_attributes = vars(instance)
+        db_attributes = [f"'{value}'" for key, value in instance_attributes.items() if
                          not key.startswith('__') and 'id' not in key]
         return separator.join(db_attributes)
 
-    def _generate_update_set_string(self, instance) -> str:
+    def _generate_update_set_string(self, instance: BaseModel) -> str:
         separator = ', '
-        model_attributes = vars(instance)
-        db_attributes = [f"{key} = '{value}'" for key, value in model_attributes.items() if
+        instance_attributes = vars(instance)
+        db_attributes = [f"{key} = '{value}'" for key, value in instance_attributes.items() if
                          not key.startswith('__') and 'id' not in key]
         return separator.join(db_attributes)
 
     def read(self) -> list:
-        db_attributes = self._get_db_attributes_names()
         list_items = []
-        query = f'SELECT {db_attributes} FROM {self.table_name}'
-        result = select_query(query)
+        query = f'SELECT * FROM {self.table_name}'
+        result = self.select_all_query(query)
         for item in result:
-            item = list(item)
-            id = item.pop(0)
-            item_instance = self.model(*item, id)
+            item_instance = self.model(**item)
             list_items.append(item_instance)
         return list_items
 
-    def write(self, instance) -> None:
-        db_attributes_no_id = self._get_db_attributes_names_without_id()
+    def read_by_id(self, id: int) -> BaseModel:
+        query = f"SELECT * FROM {self.table_name} WHERE id = '{id}'"
+        result = self.select_one_query(query)
+        item_instance = self.model(**result)
+        return item_instance
+
+    def write(self, instance: BaseModel) -> None:
+        db_attributes_no_id = self._get_db_columns_names_without_id()
         query = f"""
                     INSERT into {self.table_name}({db_attributes_no_id}) 
                     VALUES ({self._generate_write_values_string(instance)})
                 """
-        execute_query(query)
+        self.execute_query(query)
 
-    def update(self, instance) -> None:
+    def update(self, instance: BaseModel) -> None:
         query = f"UPDATE {self.table_name} SET {self._generate_update_set_string(instance)} " \
                 f"WHERE id = '{instance.id}'"
-        execute_query(query)
+        self.execute_query(query)
 
     def delete(self, id: int) -> None:
         query = f"DELETE FROM {self.table_name} WHERE id = '{id}'"
-        execute_query(query)
+        self.execute_query(query)
