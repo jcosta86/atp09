ac1ccea82bd7c56324482c7541fec31ca431351a
Matheus Abreu <matheus.abreu@olist.com>
2021-01-14 23:42:37 -0300

    Implemented BaseModel

--- backend/models/log_model.py
+++ backend/models/log_model.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from backend.models.base_model import BaseModel
 
 
@@ -5,6 +7,7 @@ class Log(BaseModel):
     __tablename__ = 'logfile'
     id = None
     activity = str()
+    domain_activity = str()
     date_activity = str()
     time_activity = str()
 
@@ -15,3 +18,7 @@ class Log(BaseModel):
         self.domain_activity = domain_activity
         self.date_activity = date_activity
         self.time_activity = time_activity
+        if date_activity is None:
+            self.date_activity = datetime.now().date()
+        if time_activity is None:
+            self.time_activity = datetime.now().time()
--- backend/models/seller_model.py
+++ backend/models/seller_model.py
@@ -1,4 +1,13 @@
-class Seller:
+from backend.models.base_model import BaseModel
+
+
+class Seller(BaseModel):
+    __tablename__ = 'seller'
+    id = None
+    fullname = str()
+    email = str()
+    phone = str()
+
     def __init__(self, fullname: str, email: str, phone: str, id: int = None):
         self.id = id
         self.fullname = fullname
