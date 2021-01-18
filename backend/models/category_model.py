8cb70c420866b9d3a764dcd7cbdc50584ed8f508
josue-avila <josue.avila@olist.com>
2021-01-18 14:15:06 -0300

    add property and setters to product and category

--- backend/models/category_model.py
+++ backend/models/category_model.py
@@ -3,11 +3,25 @@ from backend.models.base_model import BaseModel
 
 class Category(BaseModel):
     __tablename__ = 'category'
-    id = None
-    name = str()
-    description = str()
 
     def __init__(self, name: str, description: str, id: int = None) -> None:
-        self.id = id
-        self.name = name
-        self.description = description
+        super().__init__(id)
+        self.__id = id
+        self.__name = name
+        self.__description = description
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def description(self) -> str:
+        return self.__description
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self.__name = value
+
+    @description.setter
+    def description(self, value: str) -> None:
+        self.__description = value
--- backend/models/product_model.py
+++ backend/models/product_model.py
@@ -3,13 +3,33 @@ from backend.models.base_model import BaseModel
 
 class Product(BaseModel):
     __tablename__ = 'product'
-    id = None
-    name = str()
-    description = str()
-    price = float()
 
     def __init__(self, name: str, description: str, price: float, id: int = None) -> None:
-        self.id = id
-        self.name = name
-        self.description = description
-        self.price = price
+        super().__init__(id)
+        self.__name = name
+        self.__description = description
+        self.__price = price
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def description(self) -> str:
+        return self.__description
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self.__name = value
+
+    @description.setter
+    def description(self, value: str) -> None:
+        self.__description = value
+    
+    @price.setter
+    def price(self, value: float) -> None:
+        self.__price = value
