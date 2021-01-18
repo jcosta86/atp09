34803cefd3d1019f122fc9ea1039da5575d23ad8
Matheus Abreu <matheus.abreu@olist.com>
2021-01-18 13:13:29 -0300

    Refactored seller_model to use properties

--- backend/models/seller_model.py
+++ backend/models/seller_model.py
@@ -3,13 +3,33 @@ from backend.models.base_model import BaseModel
 
 class Seller(BaseModel):
     __tablename__ = 'seller'
-    id = None
-    fullname = str()
-    email = str()
-    phone = str()
 
     def __init__(self, fullname: str, email: str, phone: str, id: int = None):
-        self.id = id
-        self.fullname = fullname
-        self.email = email
-        self.phone = phone
+        super().__init__(id)
+        self.__fullname = fullname
+        self.__email = email
+        self.__phone = phone
+
+    @property
+    def fullname(self) -> str:
+        return self.__fullname
+
+    @fullname.setter
+    def fullname(self, value: str):
+        self.__fullname = value
+
+    @property
+    def email(self) -> str:
+        return self.__email
+
+    @email.setter
+    def email(self, value: str):
+        self.__email = value
+
+    @property
+    def phone(self) -> str:
+        return self.__phone
+
+    @phone.setter
+    def phone(self, value: str):
+        self.__phone = value
