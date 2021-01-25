69b630b2781cb57a1959fcb30608161f17a6a5e5
Derli Machado <dam.machado1@gmail.com>
2021-01-25 14:48:38 -0300

    Refactor: tests in marketplace.

--- backend/test/models/marketplace_model_test.py
+++ backend/test/models/marketplace_model_test.py
@@ -1,10 +1,22 @@
 from backend.models.marketplace_model import Marketplace
 
 
-marketplace = Marketplace('Izi', 'Izi izi')
-assert type(marketplace) == Marketplace
-assert isinstance(marketplace, Marketplace)
-assert marketplace.name == 'Izi'
-assert marketplace.description == 'Izi izi'
-assert type(marketplace.name) == str
-assert type(marketplace.description) == str
+class TestMarketplaceModel:
+    marketplace_name = 'marketplace'
+    marketplace_description = 'marketplace description'
+    marketplace_id = 8
+
+    def test_atributes(self) -> None:
+        marketplace = Marketplace(self.marketplace_name, self.marketplace_description, self.marketplace_id)
+        assert isinstance(marketplace, Marketplace)
+        assert marketplace.name is self.marketplace_name
+        assert marketplace.description is self.marketplace_description
+        assert marketplace.id is self.marketplace_id
+
+
+def start_test_marketplace_model():
+    test_marketplace_model = TestMarketplaceModel()
+    test_marketplace_model.test_atributes()
+
+
+start_test_marketplace_model()
