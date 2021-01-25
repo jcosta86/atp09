174372ed99b12b00dd0d6839da2b57c2283c086a
jcosta86 <jeferson.costa@olist.com>
2021-01-25 12:23:12 -0300

    Seller Orm

--- backend/dao/seller_dao.py
+++ backend/dao/seller_dao.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('.')
+
 from backend.dao.base_dao import BaseDao
 from backend.models.seller_model import Seller
 
@@ -6,13 +9,3 @@ class SellerDao(BaseDao):
     def __init__(self):
         super().__init__(Seller)
 
-    def create_table(self) -> None:
-        query = f'''CREATE TABLE IF NOT EXISTS {self.table_name} (
-                id serial NOT NULL,
-                fullname varchar NOT NULL,
-                phone varchar NOT NULL,
-                email varchar NOT NULL,
-                CONSTRAINT {self.table_name}_pk PRIMARY KEY (id)
-                );
-                '''
-        self.execute_query(query)
