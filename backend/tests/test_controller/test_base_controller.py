427d501f9913f5ac4d50cf9f2855ca35b9b525e3
jcosta86 <jeferson.costa@olist.com>
2021-01-26 09:47:37 -0300

    testando base controller

---
+++ backend/tests/test_controller/test_base_controller.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append('.')
+
+from backend.controllers.base_controller import BaseController
+from backend.models.marketplace_model import Marketplace
+from backend.dao.marketplace_dao import MarketplaceDao
+from backend.dao.session import Session
+
+
+class TestBaseController:
+    marketplace_dao = MarketplaceDao()
+    base_controller = BaseController(marketplace_dao, 'marketplace')
+    name = '!@##$#!#@!'
+    description = 'Test Marketplace Controller'
+    id = None
+    
+    def test_save(self) -> None:
+        mkt = Marketplace(self.name, self.description)
+        self.base_controller.write(mkt)
+        with Session() as session:
+            object_test = session.query(Marketplace).filter_by(name=self.name).first()
+        assert self.name == object_test.name
+        self.id = object_test.id
+    
+    def test_read_all(self) -> None:
+        marketplaces = self.base_controller.read()
+        assert isinstance(marketplaces, list)
+        
+    def test_read_by_id(self) -> None:
+        marketplace = self.base_controller.read_by_id(self.id)
+        assert marketplace.id == self.id
+        assert isinstance(marketplace, Marketplace)
+    
+    def test_update(self) -> None:
+        marketplace = self.base_controller.read_by_id(self.id)
+        description = marketplace.description
+        marketplace.description = 'Test Update Marketplace controller'
+        self.base_controller.update(marketplace)
+        marketplace_ = self.base_controller.read_by_id(self.id)
+        assert marketplace_.description != description
+
+    def test_delete(self) -> None:
+        marketplace = self.base_controller.read_by_id(self.id)
+        self.base_controller.delete(marketplace)
+        marketplace_after_delete = self.base_controller.read_by_id(self.id)
+        assert marketplace_after_delete is None
+        
+        
+def start_base_controller_tests() -> None:
+    test_controller = TestBaseController()
+    test_controller.test_save()
+    test_controller.test_read_all()       
+    test_controller.test_read_by_id()       
+    test_controller.test_update()       
+    test_controller.test_delete()
+    
+start_base_controller_tests()
